930bef960ce19825c13865bd0e892e31c2ca3a71
varrivoda <86134427+varrivoda@users.noreply.github.com>
2025-05-09 02:29:39 +1000

    Pre-intergation to Jmix: +read config file with client and secret and path to jmix API +pulling active tasks from current project (and send to a tiny Node test server on port3000 (included))
    new file:   ConfigFile.txt
    new file:   JsonConverter.bas
    modified:   msp-demo-30min.mpp
    modified:   readme.md
    new file:   testServer/TestServer.js

---
+++ testServer/TestServer.js
@@ -0,0 +1,28 @@
+﻿const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+
+app.post('/msp/issue', (req, res) => {
+    const inputString = req.body
+
+    // if (!inputString) {
+	// console.log('Status 400: Нет строки для обработки');
+    //     return res.status(400).json({ error: 'Нет строки для обработки' });
+    // }
+    //
+    console.log(inputString);
+    //
+    // // Возвращаем массив в формате JSON
+     return res.status(201).json(inputString);
+    //return inputString
+})
+
+
+app.listen(3000)
+//     port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
